d045b2b10e74a604867386523a45fe73a9fdd7cf
saurabh <emosrb@gmail.com>
2021-10-30 22:48:13 -0500

    16. Step 04 - Handling click event on the increment button

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -4,11 +4,15 @@ class Counter extends Component{
     render() {
         return(
             <div className="counter">
-                 <button>+1</button>
+                 <button onClick={increment}>+1</button>
                 <span className="count">0</span>
             </div>
         );
     }
+}
+
+function increment(){
+    console.log("increment")
 }
 
 export default Counter;
