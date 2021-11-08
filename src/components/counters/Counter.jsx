22fb72462e009d850f24ce0cc8ee55679d6c0fdd
saurabh <emosrb@gmail.com>
2021-11-07 20:18:25 -0600

    17. Step 05 - Adding State to a React Counter Component -> Counter increment

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -1,18 +1,31 @@
 import {Component} from "react";
 import './counter.css';
 class Counter extends Component{
+
+    constructor() {
+        super();
+        this.state={
+            counter:0
+        }
+        this.increment = this.increment.bind(this);
+    }
+
     render() {
         return(
             <div className="counter">
-                 <button onClick={increment}>+1</button>
-                <span className="count">0</span>
+                 <button onClick={this.increment}>+1</button>
+                <span className="count">{this.state.counter}</span>
             </div>
         );
     }
-}
 
-function increment(){
-    console.log("increment")
+     increment(){
+         this.setState({
+            counter: this.state.counter+1
+         });
+    }
+
+
 }
 
 export default Counter;
