1bb63057b0af99a425f1d564ad09f9ce91c51a5d
saurabh <emosrb@gmail.com>
2021-11-10 00:38:55 -0600

    20. Step 08 - Defining Inline Javascript CSS in JSX
    Style in jsx

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -5,23 +5,27 @@ class Counter extends Component{
     constructor() {
         super();
         this.state={
-            counter:0
+            counter : 0,
+            secondCounter : 100
         }
         this.increment = this.increment.bind(this);
     }
 
     render() {
+        const style = {fontSize:"50px", padding:"15px 30px"};
         return(
             <div className="counter">
-                 <button onClick={this.increment}>+1</button>
-                <span className="count">{this.state.counter}</span>
+                <button onClick={this.increment}>+1</button>
+                <span className="count" style={style}>{this.state.counter}</span>
+                <span className="count">{this.state.secondCounter}</span>
             </div>
         );
     }
 
      increment(){
          this.setState({
-            counter: this.state.counter+1
+            counter: this.state.counter+1,
+             secondCounter: this.state.secondCounter+1
          });
     }
 
