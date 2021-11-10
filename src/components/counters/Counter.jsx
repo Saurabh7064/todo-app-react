10c277ca97c51458799158cef3fadad614729e78
saurabh <emosrb@gmail.com>
2021-11-10 01:04:08 -0600

    21. Step 09 - Defining Multiple Counter Buttons and Adding a Component Prop
    Passing prop in react

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -5,8 +5,7 @@ class Counter extends Component{
     constructor() {
         super();
         this.state={
-            counter : 0,
-            secondCounter : 100
+            counter : 0
         }
         this.increment = this.increment.bind(this);
     }
@@ -15,17 +14,15 @@ class Counter extends Component{
         const style = {fontSize:"50px", padding:"15px 30px"};
         return(
             <div className="counter">
-                <button onClick={this.increment}>+1</button>
+                <button onClick={this.increment}>+{this.props.by}</button>
                 <span className="count" style={style}>{this.state.counter}</span>
-                <span className="count">{this.state.secondCounter}</span>
             </div>
         );
     }
 
      increment(){
          this.setState({
-            counter: this.state.counter+1,
-             secondCounter: this.state.secondCounter+1
+            counter: this.state.counter+ this.props.by
          });
     }
 
