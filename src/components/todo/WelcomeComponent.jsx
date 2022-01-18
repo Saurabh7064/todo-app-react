3a6f47f7286f5ba4607f80e097b933cb44ed270c
saurabh <emosrb@gmail.com>
2022-01-18 12:48:02 +0530

    67. Step 34 - Connecting React with Restful API - 4 - Adding Axios and Creating Hel

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import HelloWorldService from "../../api/todo/HelloWorldService";
 
 class WelcomeComponent extends Component{
+
+    constructor(props) {
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+    }
+
     render(){
         return(
             <>
@@ -10,9 +17,22 @@ class WelcomeComponent extends Component{
                     Welcome {this.props.match.params.name}.
                     You can manage your todos <Link to="/todos">here</Link>.
                 </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage}
+                            className="btn btn-success">Get Welcome Message</button>
+                </div>
             </>
         )
     }
+
+    retrieveWelcomeMessage() {
+        console.log("clicked");
+        HelloWorldService.executeHelloWorldService()
+            .then(response=>console.log(response));
+    }
 }
+
+
 
 export default WelcomeComponent
