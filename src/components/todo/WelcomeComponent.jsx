36ae829812fca8f69b710e73e42cde37f7e1b7d7
saurabh <emosrb@gmail.com>
2022-01-20 15:24:24 +0530

    68. Step 35 - Connecting React with Restful API - 5 - Allow Cross Origin Requests Fr

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -2,11 +2,16 @@ import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import HelloWorldService from "../../api/todo/HelloWorldService";
 
-class WelcomeComponent extends Component{
+class WelcomeComponent extends  Component{
 
     constructor(props) {
         super(props)
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+        this.state = {
+            welcomeMessage: ''
+        }
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
     }
 
     render(){
@@ -22,14 +27,44 @@ class WelcomeComponent extends Component{
                     <button onClick={this.retrieveWelcomeMessage}
                             className="btn btn-success">Get Welcome Message</button>
                 </div>
+                <div className="container">
+                     {this.state.welcomeMessage}
+                </div>
             </>
         )
     }
 
     retrieveWelcomeMessage() {
-        console.log("clicked");
         HelloWorldService.executeHelloWorldService()
-            .then(response=>console.log(response));
+        .then( response => this.handleSuccessfulResponse(response) )
+
+        // HelloWorldService.executeHelloWorldBeanService()
+        // .then( response => this.handleSuccessfulResponse(response) )
+
+        // HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
+        //     .then(response => this.handleSuccessfulResponse(response))
+        //     .catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response) {
+        console.log(response)
+        this.setState({ welcomeMessage: response.data })
+    }
+
+    handleError(error) {
+
+        console.log(error.response)
+
+        let errorMessage = '';
+
+        if (error.message)
+            errorMessage += error.message
+
+        if (error.response && error.response.data) {
+            errorMessage += error.response.data.message
+        }
+
+        this.setState({ welcomeMessage: errorMessage })
     }
 }
 
